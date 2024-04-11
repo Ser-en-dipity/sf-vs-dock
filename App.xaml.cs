b6aed9750cd8514c676464ac2df1f8f2535bf570
Ser-en-dipity <610511058@qq.com>
2024-04-11 21:39:17 +0800

    init

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace wpf_vs_docking;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NBaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXtfcXRXQmhcVUBwWUU=");
+    }
+}
+
